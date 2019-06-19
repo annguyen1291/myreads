bab39bcff842e732d3c8c02d38323b2c28cc8dd6
An Nguyen <an@pestroutes.com>
2019-06-19 13:45:46 -0500

    main-page-1
    The main page shows 3 shelves for books. Each book is shown on the correct shelf, along with its title and all of its authors.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBook from './SearchBook'
 import ListBooks from './ListBooks'
 import { Route } from 'react-router-dom' 
 
 class BooksApp extends React.Component {
-  state = {
+  state = { 
+    books: ['currentlyReading', 'wantToRead', 'read']
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState(() => ({
+          books
+        }))
+      })
+  }
+
+  updateBook = (book, shelf) => {
+    this.setState((state) => ({
+      books: state.books.filter((b) => {
+        return b.id !== book.id
+      })
+    }))
+
+    BooksAPI.update(book, shelf)
   }
 
   render() {
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-          <ListBooks />
+          <ListBooks books={this.state.books}/>
         )} />
         <Route path='/search' render={({ history }) => (
           <SearchBook 
